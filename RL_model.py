70677035276607b7db68c9309b15b992e029282e
sivginirmak <sivginirmak@gmail.com>
2024-12-07 15:19:05 -0800

    update train

--- RL_model.py
+++ RL_model.py
@@ -76,7 +76,8 @@ BATCH_SIZE = 128
 GAMMA = 1
 TAU = 0.05
 LR = 1e-4
-
+LOWx, HIx = -0.5, 0.5
+LOWz, HIz = 0., 1.
 
 n_actions = 101**2 ### must check but for now: 0, 0.1, ..., 0.9, 1
 state_dim = 10 # 
@@ -86,13 +87,13 @@ target_net = DQN(state_dim, n_actions).to(device)
 target_net.load_state_dict(policy_net.state_dict())
 
 optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
-memory = ReplayMemory(10000)
+memory = ReplayMemory(50000)
+print(len(memory))
 
-
-def process_data():
+def process_data(datacsv):
 
     # df = pd.read_csv("stacking_samples_mixed_p1.0.csv")
-    df = pd.read_csv("train_36149_p0.2.csv")
+    df = pd.read_csv(datacsv)
     # df = df[:20]
 
     state_headers = ["box_0_x","box_0_z","box_0_l","box_0_h","box_c_l","box_c_h","box_1_x","box_1_z","box_1_l","box_1_h"]
@@ -104,21 +105,17 @@ def process_data():
         if i + 1 < len(df):  # Ensure there's a next line for the pair
             # Extract state, action, and next_state (2nd box placement, even rows)
             state = df.loc[i, state_headers].values.tolist()
-            # state = np.clip(state, 0, 1) # make sure you stay within bounds
-            # print(np.max(state), np.min(state))
             action = df.loc[i, action_headers].values.tolist()
             action = np.clip(action, 0, 1)
             next_state = df.loc[i + 1, state_headers].values.tolist()
-            # next_state = np.clip(next_state, 0, 1)
             reward = df.loc[i, reward_headers].values.tolist()
 
             memory.push(torch.tensor(state), torch.tensor(action), torch.tensor(next_state), torch.tensor(reward))
 
             # Extract state, action, and next_state (3rd box placement, odd rows)
             state = df.loc[i + 1, state_headers].values.tolist()
-            # state = np.clip(state, 0, 1)
             action = df.loc[i + 1, action_headers].values.tolist()
-            action = np.clip(action, 0, 1)
+            # action = np.clip(action, 0, 1)
             # next_state fill with None 
             reward = df.loc[i + 1, reward_headers].values.tolist()
 
@@ -129,27 +126,26 @@ def process_data():
 
 
 
-steps_done = 0
 
 
-
-def coord2ind(action, low=-0.5, high=0.5):
-    width = high - low
+def coord2ind(action):
+    # equal width x and z
+    width = HIx - LOWx
     num = int(width/RESOLUTION)
 
     rounded_action = np.round(action / RESOLUTION) * RESOLUTION
-    indx = (((rounded_action[:,0] - low) / width) * num).int() # 0 to num
-    indz = (((rounded_action[:,1] - low) / width) * num).int() # 0 to num
+    indx = (((rounded_action[:,0] - LOWx) / width) * num).int() # 0 to num
+    indz = (((rounded_action[:,1] - LOWz) / width) * num).int() # 0 to num
 
     return np.ravel_multi_index([indx, indz], (num + 1 , num + 1)) 
 
-def ind2coord(action_index, low=-0.5, high=0.5):
+def ind2coord(action_index):
     
-    width = high - low
+    width = HIx - LOWx
     num = int(width/RESOLUTION)
     coord = np.unravel_index(action_index, (num+1, num+1))
-    ax = (coord[0] / num) * width + low
-    az = (coord[1] / num) * width + low
+    ax = (coord[0] / num) * width + LOWx
+    az = (coord[1] / num) * width + LOWz
     return np.array([ax, az]).T # batch, 2
 
 
@@ -164,35 +160,24 @@ def optimize_model(transitions):
     # to Transition of batch-arrays.
     batch = Transition(*zip(*transitions))
 
-    # transitions = memory.sample(BATCH_SIZE)
-    # batch = Transition(*zip(*transitions))
-    # print(batch)
-
-
     # Compute a mask of non-final states and concatenate the batch elements
     # (a final state would've been the one after which simulation ended)
     non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
                                           batch.next_state)), device=device, dtype=torch.bool)
     non_final_next_states = torch.stack([s for s in batch.next_state if s is not None]).to(device)
 
-    # print(batch.state)
-
     state_batch = torch.stack(batch.state).to(device)
     action_batch = torch.stack(batch.action)
-    # ipdb.set_trace()
+
     action_batch_idx = torch.from_numpy(coord2ind(action_batch)).to(device)
-    # action_batch = action_batch.to(device)
-    
+
     reward_batch = torch.stack(batch.reward).to(device)
 
     # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
     # columns of actions taken. These are the actions which would've been taken
     # for each batch state according to policy_net
-    # print(policy_net(state_batch))
-    # print("net output: \n", policy_net(state_batch).shape)
-    # ipdb.set_trace()
+
     state_action_values = policy_net(state_batch).gather(1, action_batch_idx.unsqueeze(-1))
-    # print("state_action_values:\n", state_action_values)
 
     # Compute V(s_{t+1}) for all next states.
     # Expected values of actions for non_final_next_states are computed based
@@ -201,15 +186,14 @@ def optimize_model(transitions):
     # state value or 0 in case the state was final.
     next_state_values = torch.zeros(state_batch.shape[0], device=device)
     with torch.no_grad():
-        # print(next_state_values[non_final_mask].shape)
-        # print(target_net(non_final_next_states).shape)
+
         next_state_values[non_final_mask] = target_net(non_final_next_states).max(1).values
     # Compute the expected Q values
     expected_state_action_values = (next_state_values.unsqueeze(-1) * GAMMA) + reward_batch
 
     # Compute Huber loss
-    # criterion = nn.SmoothL1Loss()
-    criterion = nn.MSELoss()
+    criterion = nn.SmoothL1Loss()
+    # criterion = nn.MSELoss()
     loss = criterion(state_action_values, expected_state_action_values)
 
     # Optimize the model
@@ -222,10 +206,11 @@ def optimize_model(transitions):
 
 
 def test(model):
-    df = pd.read_csv("train_100_p1.0.csv")
+    df = pd.read_csv("train_data/train_5000_p1.0_unnoised.csv")
+    df = df[:100]
     opt_actions = np.array(df[["a_x","a_z"]].values)
     opt_actions = np.round(opt_actions, 2) # 2 decimals, same as resolution 0.01
-    opt_actions = np.clip(opt_actions, 0, 1)
+    # opt_actions = np.clip(opt_actions, 0, 1)
     state_headers = ["box_0_x","box_0_z","box_0_l","box_0_h","box_c_l","box_c_h","box_1_x","box_1_z","box_1_l","box_1_h"]
 
     state = df[state_headers].values.tolist()
@@ -240,7 +225,15 @@ def test(model):
 
 num_epochs = 10#000
 
-memory = process_data()
+trainfile = "train_data/train_10000_p0.0_unnoised.csv"
+memory = process_data(trainfile)
+print(len(memory))
+
+trainfile = "train_data/train_5000_p1.0_unnoised.csv"
+memory = process_data(trainfile)
+print(len(memory))
+
+ipdb.set_trace()
 losses = []
 for i_epoch in trange(num_epochs, desc="Training Epochs"):
     batches = memory.shuffle_and_sample(BATCH_SIZE)
@@ -259,19 +252,19 @@ for i_epoch in trange(num_epochs, desc="Training Epochs"):
             target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
         target_net.load_state_dict(target_net_state_dict)
 
-        if (ii % int(len(batches)/ 5)) == 0:
-            policy_net.eval()
-            test(policy_net)
-            target_net.eval()
-            test(target_net)
+        # if (ii % int(len(batches)/ 5)) == 0:
+        #     policy_net.eval()
+        #     test(policy_net)
+        #     target_net.eval()
+        #     test(target_net)
     # losses.append(epoch_loss / len(batches))
     # print(losses[-1])
     if (i_epoch % 5) == 0:
         print(i_epoch)
-        if i_epoch == 0:
-            torch.save(target_net.state_dict(), "target_net.pt")
-            torch.save(policy_net.state_dict(), "policy_net.pt")
-            break
+        # if i_epoch == 0:
+        #     torch.save(target_net.state_dict(), "target_net.pt")
+        #     torch.save(policy_net.state_dict(), "policy_net.pt")
+        #     break
         policy_net.eval()
         test(policy_net)
         target_net.eval()
